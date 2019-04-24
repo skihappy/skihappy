68ec6cbc38e2a8f3381352359421fe869043c868
Sierk Beij <sierk@riseup.net>
2019-04-24 00:47:37 -0700

    Refactoring into monorepo.

---
+++ .eslintrc.js
@@ -0,0 +1,30 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+  ],
+  plugins: ['import'],
+  settings: {
+    'import/resolver': {
+      node: {
+        // Allow import and resolve for *.ts modules.
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+      },
+    },
+  },
+  env: {
+    browser: false,
+    commonjs: true,
+    es6: true,
+    jest: true,
+    node: true,
+  },
+  rules: {
+    // eslint-plugin-import
+    'no-console': 'off',
+    'import/prefer-default-export': 'off',
+  },
+}
